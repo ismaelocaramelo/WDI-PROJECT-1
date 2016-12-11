ea4f9ae6c8327abef12422f59e1d0f085b82f9d7
ismaelocaramelo <ismaelbacha@hotmail.com>
2016-12-11 01:12:50 +0000

    Adding functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,446 @@
-console.log('loaded');
+
+function Player(name, color, disable){
+  var _name = name;
+  var _score = 2;
+  var _color = color; // 'B' or 'W' (black or white)
+  var _disable = disable;
+
+  this.getName = function(){
+    return _name;
+  };
+
+  this.addScore = function(){
+    _score++;
+  };
+
+  this.lessScore = function(){
+    _score--;
+  };
+
+  this.getScore = function(){
+    return _score;
+  };
+
+  this.getColor = function(){
+    return _color;
+  };
+
+  this.disable = function(){
+    _disable = true;
+  };
+  this.enable =function(){
+    _disable = false;
+  };
+  this.isDisable = function(){
+    return _disable;
+  };
+}
+
+$(init);
+
+
+var player1;
+var player2;
+var grid = [];
+const WIDTH = 8;
+const HEIGHT = 8;
+var currentTurn = 'B';
+var listPosToChangeColor = [];
+
+function init(){
+  var name1 = 'isma'; //prompt("Name of the player 1: ");
+  var name2 = 'jos';  //prompt("Name of the player 2: ");
+
+  player1 = new Player(name1,'W',false);
+  player2 = new Player(name2,'B',true);
+
+  for (var line = 0; line < HEIGHT; line++) {
+    grid[line] = new Array();
+    for (var column = 0; column < WIDTH; column++) {
+      grid[line][column] = '';
+    }
+  }
+
+  grid[HEIGHT/2-1][WIDTH/2-1] = 'W';
+  grid[HEIGHT/2-1][WIDTH/2]   = 'B';
+  grid[HEIGHT/2][WIDTH/2-1]   = 'B';
+  grid[HEIGHT/2][WIDTH/2]     = 'W';
+  drawGrid();
+}
+
+function changeTurn(){
+  listPosToChangeColor =[];
+
+  if(currentTurn === 'B'){
+    currentTurn = 'W';
+    player1.enable();
+    player2.disable();
+  }else if (currentTurn === 'W'){
+    currentTurn = 'B';
+    player2.enable();
+    player1.disable();
+  }
+}
+
+function isCellEmpty(line, column){
+  return grid[line][column] === '';
+}
+
+function isHorizontal(){}
+
+function drawGrid(){
+  var $box = $('.box');
+
+  for (var line = 0; line < HEIGHT; line++) {
+    for (var column = 0; column < WIDTH; column++) {
+      var letter = grid[line][column];
+      $box.append('<div id="'+line+'-'+column+'" class="square'+letter+'" onclick="insertMove(this.id)"></div>');
+    }
+    $box.append('</br>');
+  }
+}
+
+function isValidMoved(line, column){
+  var isValid = false;
+
+  isValid += isNorthValid(line, column);
+  isValid += isSouthValid(line, column);
+  isValid += isEastValid(line, column);
+  isValid += isOestValid(line, column);
+  isValid += isNorthEast(line, column);
+  isValid += isSouthEast(line, column);
+  isValid += isSouthOest(line, column);
+  isValid += isNorthOest(line, column);
+
+  if(!isValid){
+    listPosToChangeColor =[];
+  }
+  return isValid;
+}
+
+function insertMove(id){
+  var line = id.split('-')[0];
+  var column = id.split('-')[1];
+  if (isCellEmpty(line,column)){
+    //The cell is empty
+    if(isValidMoved(line, column)){
+      changeImage();
+      addImage(line,column);
+      changeTurn();
+    }else{
+      alert('Invalid move');
+    }
+  }else{
+    alert('Invalid move: The cell is not empty');
+  }
+}
+
+function isOpositeColor(line, column){
+  var opositeColor = (currentTurn==='B') ? 'W': 'B';
+  return (grid[line][column]===opositeColor);
+}
+
+function changeImage(){
+  for (var i = 0; i < listPosToChangeColor.length; i++) {
+    var coordenadas = listPosToChangeColor[i];
+    grid[coordenadas.line][coordenadas.column] = currentTurn;
+    var $square = $('#'+coordenadas.line+ '-'+coordenadas.column);
+    $square.attr('class', 'square'+currentTurn);
+    if(currentTurn === 'B'){
+      player2.addScore();
+      player1.lessScore();
+    }else if(currentTurn === 'W'){
+      player1.addScore();
+      player2.lessScore();
+    }
+  }
+}
+
+function addImage(line,column){
+  grid[line][column] = currentTurn;
+  var $square = $('#'+line+ '-'+column); //document.getElementById(line+ '-'+column)
+  $square.attr('class', 'square'+currentTurn);
+  if(currentTurn === 'B'){
+    player2.addScore();
+  }else if(currentTurn === 'W'){
+    player1.addScore();
+  }
+
+}
+
+function isNorthValid(line, column){
+  var listTmpPos = [];
+ //para que el movimiento sea valido
+ //linea tiene que estar en el dominio:  ]1,HEIGHT[
+  if(line>1 && line<HEIGHT){
+  //Linea esta en el dominio ]1,HEIGHT[
+  //Comprobamos que por encima haya minimo una casilla con el valor contrario;
+    var cntOpositeColor=0;
+    line--;
+
+    while(line>=0 && isOpositeColor(line,column)){
+      cntOpositeColor++;
+      listTmpPos.push({'line': line,'column': column});
+      line--;
+    }
+
+    if(cntOpositeColor>0){
+   //si entramos aqui significa que hemos
+   //encontrado 1 o más fichas del color opuesto
+
+   //En caso de ser así.
+   //ya que hemos ido haciendo line--
+   //Comprobamos que en la posición de line actual
+   //el color de la casilla sea igual al de nuestro turno.
+      if(line>=0 && grid[line][column]===currentTurn){
+        listPosToChangeColor = listPosToChangeColor.concat(listTmpPos);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+   //En caso de no haber encontrado ninguna ficha del color opuesto
+   //El movimiento no es valido
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+function isSouthValid(line, column){
+  var listTmpPos = [];
+  //para que el movimiento sea valido
+  //linea tiene que estar en el dominio:  ]0,HEIGHT[
+  if(line>=0 && line<HEIGHT-2){
+   //Linea esta en el dominio ]1,HEIGHT[
+   //Comprobamos que por encima haya minimo una casilla con el valor contrario;
+    var cntOpositeColor=0;
+    line++;
+
+    while(line<HEIGHT && isOpositeColor(line,column)){
+      cntOpositeColor++;
+      listTmpPos.push({'line': line,'column': column});
+      line++;
+    }
+
+    if(cntOpositeColor>0){
+    //si entramos aqui significa que hemos
+    //encontrado 1 o más fichas del color opuesto
+
+    //En caso de ser así.
+    //ya que hemos ido haciendo line--
+    //Comprobamos que en la posición de line actual
+    //el color de la casilla sea igual al de nuestro turno.
+      if(line<HEIGHT && grid[line][column]===currentTurn){
+        listPosToChangeColor = listPosToChangeColor.concat(listTmpPos);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+    //En caso de no haber encontrado ninguna ficha del color opuesto
+    //El movimiento no es valido
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+function isEastValid(line, column){
+  var listTmpPos = [];
+  //para que el movimiento sea valido
+  //linea tiene que estar en el dominio:  ]0, WIDTH[
+  if(column>=0 && column<WIDTH-2){
+   //Linea esta en el dominio ]0, WIDTH[
+   //Comprobamos que por encima haya minimo una casilla con el valor contrario;
+    var cntOpositeColor=0;
+    column++;
+
+    while(column<WIDTH && isOpositeColor(line,column)){
+      cntOpositeColor++;
+      listTmpPos.push({'line': line,'column': column});
+      column++;
+    }
+
+    if(cntOpositeColor>0){
+    //si entramos aqui significa que hemos
+    //encontrado 1 o más fichas del color opuesto
+
+    //En caso de ser así.
+    //ya que hemos ido haciendo column++
+    //Comprobamos que en la posición de column actual
+    //Comprobamos que nos nos pasemos chequando...
+      if(column<WIDTH && grid[line][column]===currentTurn){
+        listPosToChangeColor = listPosToChangeColor.concat(listTmpPos);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+    //En caso de no haber encontrado ninguna ficha del color opuesto
+    //El movimiento no es valido
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+function isOestValid(line, column){
+  var listTmpPos = [];
+  //para que el movimiento sea valido
+  //linea tiene que estar en el dominio:  ]1, WIDTH[
+  if(column>1 && column<WIDTH){
+   //Linea esta en el dominio ]0, WIDTH[
+   //Comprobamos que por encima haya minimo una casilla con el valor contrario;
+    var cntOpositeColor=0;
+    column--;
+
+    while(column>=0 && isOpositeColor(line,column)){
+      cntOpositeColor++;
+      listTmpPos.push({'line': line,'column': column});
+      column--;
+    }
+
+    if(cntOpositeColor>0){
+    //si entramos aqui significa que hemos
+    //encontrado 1 o más fichas del color opuesto
+
+    //En caso de ser así.
+    //ya que hemos ido haciendo column--
+    //Comprobamos que en la posición de column actual
+    //el color de la casilla sea igual al de nuestro turno.
+      if(column>=0 && grid[line][column]===currentTurn){
+        listPosToChangeColor = listPosToChangeColor.concat(listTmpPos);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+    //En caso de no haber encontrado ninguna ficha del color opuesto
+    //El movimiento no es valido
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+function isNorthEast(line, column){
+  var listTmpPos = [];
+  if((line>1) && (line<HEIGHT) && (column>=0) && (column<WIDTH-2)){
+
+    var cntOpositeColor=0;
+    line--;
+    column++;
+
+    while((line>=0) && (column <WIDTH) && isOpositeColor(line, column)){
+      cntOpositeColor++;
+      listTmpPos.push({'line': line,'column': column});
+      line--;
+      column++;
+    }
+    if(cntOpositeColor>0){
+      if( line>=0 && column<WIDTH && grid[line][column]===currentTurn){
+        listPosToChangeColor = listPosToChangeColor.concat(listTmpPos);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+function isNorthOest(line, column){
+  var listTmpPos = [];
+
+  if((line>1) && (line<HEIGHT) && (column>1) && (column<WIDTH)){
+
+    var cntOpositeColor=0;
+    line--;
+    column--;
+
+    while( (line>=0) && (column>=0) && isOpositeColor(line, column)){
+      cntOpositeColor++;
+      listTmpPos.push({'line': line,'column': column});
+      line--;
+      column--;
+    }
+    if(cntOpositeColor>0){
+      if( (line>=0 && column>=0) && grid[line][column]===currentTurn){
+        listPosToChangeColor = listPosToChangeColor.concat(listTmpPos);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+function isSouthOest(line, column){
+  var listTmpPos = [];
+
+  if((line>=0) && (line<HEIGHT-2) && (column>1) && (column<WIDTH)){
+
+    var cntOpositeColor=0;
+    line++;
+    column--;
+
+    while( (line<HEIGHT) && (column>=0) && isOpositeColor(line, column)){
+      cntOpositeColor++;
+      listTmpPos.push({'line': line,'column': column});
+      line++;
+      column--;
+    }
+    if(cntOpositeColor>0){
+      if( (line<HEIGHT && column>=0) && grid[line][column]===currentTurn){
+        listPosToChangeColor = listPosToChangeColor.concat(listTmpPos);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+function isSouthEast(line, column){
+  var listTmpPos = [];
+  if((line>=0) && (line<HEIGHT-2) && (column>=0) && (column<WIDTH-2)){
+    var cntOpositeColor=0;
+    line++;
+    column++;
+
+    while((line<HEIGHT) && (column<WIDTH)  && isOpositeColor(line, column)){
+      cntOpositeColor++;
+      listTmpPos.push({'line': line,'column': column});
+      line++;
+      column--;
+    }
+    if(cntOpositeColor>0){
+      if( (line<HEIGHT) && (column<WIDTH)  && grid[line][column]===currentTurn){
+        listPosToChangeColor = listPosToChangeColor.concat(listTmpPos);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
